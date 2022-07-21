1f28d1c2760a1347a54d0a75242a5d87886350f5
Subramaniam Balasegaran <blsgrn@protonmail.com>
2022-07-21 10:29:57 +0530

    Add onSubmit fire off a function

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdEmail } from "react-icons/md";
 import { FaUnlockAlt } from "react-icons/fa";
-import { Button } from "@material-tailwind/react";
+import { CgProfile } from "react-icons/cg";
 import Logo from "../components/Logo";
 import { Link } from "react-router-dom";
 
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 function Register() {
+  const [values, setValues] = useState(initialState);
+  const onChange = (e) => {
+    console.log(e.targt);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+  };
+
   return (
     <div className="flex justify-center items-center h-screen overflow-hidden bg-brown-50">
-      <form className="container rounded-lg shadow-md max-w-md flex flex-col justify-center items-center bg-green-200 p-20 md:p-10 sm:p-5">
+      <form
+        onSubmit={onSubmit}
+        className=" container rounded-lg shadow-md max-w-md flex flex-col justify-center items-center bg-green-200 p-20 md:p-10 sm:p-5"
+      >
         <div className="p-5">
           <Logo />
         </div>
@@ -17,6 +35,18 @@ function Register() {
         </div>
         <div className="inline-flex bg-gray-100 rounded border border-gray-200 mt-10">
           <div className="inline bg-gray-200 py-2 px-4 text-gray-600">
+            <CgProfile size={28} color={"grey"} />
+          </div>
+          <input
+            type="text"
+            placeholder="Name"
+            value=""
+            className="bg-green-50 sm:w-40 border-transparent py-1 text-gray-600 px-4 focus:outline-none"
+            onChange={onChange}
+          />
+        </div>
+        <div className="inline-flex bg-gray-100 rounded border border-gray-200 mt-5">
+          <div className="inline bg-gray-200 py-2 px-4 text-gray-600">
             <MdEmail size={28} color={"grey"} />
           </div>
           <input
@@ -24,6 +54,7 @@ function Register() {
             placeholder="Email"
             value=""
             className="bg-green-50 sm:w-40 border-transparent py-1 text-gray-600 px-4 focus:outline-none"
+            onChange={onChange}
           />
         </div>
         <div className="inline-flex bg-gray-100 rounded border border-gray-200 mt-5">
@@ -35,6 +66,7 @@ function Register() {
             placeholder="Password"
             value=""
             className="bg-green-50 sm:w-40 border-transparent py-1 text-gray-600 px-4 focus:outline-none"
+            onChange={onChange}
           />
         </div>
         <div>
@@ -46,15 +78,15 @@ function Register() {
               </span>
             </Link>
           </h1>
-          <Button
-            size="lg"
-            variant="gradient"
-            color="amber"
-            className="p-6 m-6"
-          >
-            Sign In
-          </Button>
         </div>
+        <button
+          size="lg"
+          variant="gradient"
+          color="amber"
+          className="btn p-6 m-6"
+        >
+          Sign In
+        </button>
       </form>
     </div>
   );
